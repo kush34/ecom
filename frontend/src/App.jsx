5206a27ed8a08e53276ea8275a1224fe8d4ad082
kush34 <chattkush34@gmail.com>
2025-05-23 22:16:36 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='border-2 border-black text-2xl'>
+      EcommerceApp     
+    </div>
+  )
+}
+
+export default App
