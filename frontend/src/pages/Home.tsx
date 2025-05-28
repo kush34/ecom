c21591e2aa222647b2fe2a4b3ee17b3319926066
kush34 <chattkush34@gmail.com>
2025-05-28 22:54:40 +0530

    added product component in components fldr

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,8 @@
-import Footer from "@/components/footer"
+import Footer from "@/components/Footer"
 import Navbar from "@/components/Navbar"
 import SearchBar from "@/components/SearchBar"
 import ImageSlider from "@/components/ImageSlider"
+import Product from "@/components/Product"
 const Home = () => {
     //Navbar
     //image slider
@@ -16,6 +17,14 @@ const Home = () => {
       <Navbar/>
       <SearchBar/>
       <ImageSlider/>
+      <div className="w-full">
+        <div className="section-title text-5xl font-bold mx-10 my-5 text-[#1d242d] flex justify-center">In Store</div>
+        <div className="w-full flex justify-center flex-wrap p-3">
+          <Product/>
+          <Product/>
+          <Product/>
+        </div>
+      </div>
       <div className="h-1/2 mt-5">
         <Footer/>
       </div>
