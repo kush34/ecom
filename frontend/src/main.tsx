4050d8edfc3db9e2a07e403e72691e818e604720
kush34 <chattkush34@gmail.com>
2025-05-28 23:13:43 +0530

    added ProductPage in pages and installed react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import {BrowserRouter ,Routes,Route} from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
+import ProductPage from './pages/ProductPage.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Product/:id" element={<ProductPage />} />
+    </Routes>
+  </BrowserRouter>
+  </StrictMode>
 )
---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom"
+
+const ProductPage = () => {
+    const {id} = useParams();
+  return (
+    <div>ProductPage {id}</div>
+  )
+}
+
+export default ProductPage
